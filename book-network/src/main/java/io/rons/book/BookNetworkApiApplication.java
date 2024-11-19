b753e70988d138a2c04c4f8544070874067f4883
Ronak Sathe <satheronak@gmail.com>
2024-11-19 21:17:20 +0530

    Remaining from Auhtentication Controller 2:05:49

--- book-network/src/main/java/io/rons/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/io/rons/book/BookNetworkApiApplication.java
@@ -2,8 +2,10 @@ package io.rons.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
