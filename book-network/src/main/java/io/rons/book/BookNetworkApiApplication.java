a5027036b8c8cd23da9e764e21ba9969bf30ad9d
Ronak Sathe <satheronak@gmail.com>
2024-11-19 15:41:17 +0530

    File INitialization with dependencies

---
+++ book-network/src/main/java/io/rons/book/BookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package io.rons.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
