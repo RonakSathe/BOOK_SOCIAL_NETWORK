4f6d4e3a3d0114245dd220c6cc6bf22b122544f1
Ronak Sathe <satheronak@gmail.com>
2024-11-22 11:17:35 +0530

    Auditing Completed

---
+++ book-network/src/main/java/io/rons/book/book/Book.java
@@ -0,0 +1,48 @@
+package io.rons.book.book;
+
+
+import io.rons.book.common.BaseEntity;
+import io.rons.book.history.BookTransactionHistory;
+import io.rons.book.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter@Setter@SuperBuilder@AllArgsConstructor
+@NoArgsConstructor
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+
+
+
+}
---
+++ book-network/src/main/java/io/rons/book/book/Feedback.java
@@ -0,0 +1,22 @@
+package io.rons.book.book;
+
+import io.rons.book.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity@AllArgsConstructor@NoArgsConstructor
+@Getter@Setter@SuperBuilder
+public class Feedback extends BaseEntity {
+
+    private Double note; // 1-5 stars
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+}
